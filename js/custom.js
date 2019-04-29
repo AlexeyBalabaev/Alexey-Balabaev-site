89bdf83a230964e18aba7d7e5e1356e516c59c1f
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-04-29 13:25:07 +0300

    Edits and modify, ver.0.3.3

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,42 @@
-'use strict';
+(function () {
+	'use strict';
 
-document.querySelector('.btn').onmousemove = e => {
+	//Scroll to up
 
-  const x = e.pageX - e.target.offsetLeft;
-  const y = e.pageY - e.target.offsetTop;
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
 
-  e.target.style.setProperty('--x', `${x}px`);
-  e.target.style.setProperty('--y', `${y}px`);
+    if (scrolled > coords) {
+      goTopBtn.classList.add('back-top-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('back-top-show');
+    }
+  }
 
-};
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -30);
+      setTimeout(backToTop, 0);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.back-top');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+
+  //Animation for button
+
+	document.querySelector('.btn').onmousemove = e => {
+
+	  const x = e.pageX - e.target.offsetLeft;
+	  const y = e.pageY - e.target.offsetTop;
+
+	  e.target.style.setProperty('--x', `${x}px`);
+	  e.target.style.setProperty('--y', `${y}px`);
+
+	};
+
+})();
